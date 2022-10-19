edfb7a574ccac11de76cc834b8801f3b4037c68c
SSPENELOPE <typoepping@gmail.com>
2022-10-19 19:58:42 -0400

    stash

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -7,13 +7,4 @@ router.use("/users", userRoutes);
 router.use("/comment", commentRoutes);
 router.use("/review", reviewRoutes);
 
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const commentRoutes = require('./commentRoutes');
-const reviewRoutes = require('./reviewRoutes');
-
-router.use('/users', userRoutes);
-router.use('/comment', commentRoutes);
-router.use('/review', reviewRoutes);
-
 module.exports = router;
