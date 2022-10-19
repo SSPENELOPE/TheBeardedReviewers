deb24d797f752e0853bcf772343cfccd245b993c
Keith Gray <108160378+actionboy6@users.noreply.github.com>
2022-10-19 18:55:53 -0500

    fixing merge issues
    merge conflicts

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,9 @@
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
+const router = require("express").Router();
+const userRoutes = require("./userRoutes");
+const commentRoutes = require("./commentRoutes");
+const reviewRoutes = require("./reviewRoutes");
 
-router.use('/users', userRoutes);
-
+router.use("/users", userRoutes);
+router.use("/comment", commentRoutes);
+router.use("/review", reviewRoutes);
 module.exports = router;
